22dd4e385d2db2ccf5e09a8e9b349afcbd4e9968
April <aprilyoung010@gmail.com>
2018-08-23 12:44:59 +0800

    polish the sh

--- Image-Captioning-Project/download.sh
+++ Image-Captioning-Project/download.sh
@@ -1,9 +1,6 @@
 pip install -r requirements.txt
 
 mkdir "cocoapi/annotations" && mkdir "cocoapi/images"
-mkdir "cocoapi/images/train2014"
-mkdir "cocoapi/images/val2014"
-mkdir "cocoapi/images/test2014"
 
 echo "download train images"
 wget "http://images.cocodataset.org/zips/train2014.zip"
@@ -18,9 +15,9 @@ echo "download test annotations"
 wget "http://images.cocodataset.org/annotations/image_info_test2014.zip"
 
 echo "Unzip all the downloaded zip files"
-unzip -o  "train2014.zip" -d "cocoapi/images/train2014"
-unzip -o  "test2014.zip" -d "cocoapi/images/test2014"
-unzip -o  "val2014.zip" -d "cocoapi/images/val2014"
+unzip -o  "train2014.zip" -d "cocoapi/images"
+unzip -o  "test2014.zip" -d "cocoapi/images"
+unzip -o  "val2014.zip" -d "cocoapi/images"
 unzip -o  "annotations_trainval2014.zip" -d "cocoapi/annotations"
 unzip -o  "image_info_test2014.zip" -d "cocoapi/annotations"
 
